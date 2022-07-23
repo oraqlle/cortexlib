65908440264d89f1daed8ccb0329ea39ee06047a
oraqlle <41113853+oraqlle@users.noreply.github.com>
2022-07-23 16:01:54 +1000

    Added box_print header

---
+++ libs/box/src/box_print.hpp
@@ -0,0 +1,21 @@
+/// -*- C++ -*- Header compatibility <box.hpp>
+
+/// \brief Two Dimensional Access To Contiguous Data.
+///
+/// Author: Tyler Swann (oraqlle@github.com)
+/// 
+/// Header Version: v2.3.3
+///
+/// Date: 25-06-2022
+///
+/// License: MIT
+///
+/// Copyright: Copyright (c) 2022
+/// \file box.hpp
+
+#ifndef CORTEX_BOX_PRINT_H
+#define CORTEX_BOX_PRINT_H 1
+
+#include <box.hpp>
+
+#endif  /// CORTEX_BOX_PRINT_H
